48fff7c9e7dc1098a2267b9997dd1aa5dd075ac7
MaxxZahar <maxxzahar@rambler.ru>
2024-08-28 22:32:31 +0300

    menu

---
+++ data/systems.py
@@ -0,0 +1,7 @@
+import os
+from environs import Env
+
+env = Env()
+env.read_env()
+
+SYSTEMS = {'crazy': {'admin': [int(admin) for admin in env.list('ADMINS')][0], 'path': os.path.normpath('public/systems/crazy.csv')}}
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import logging
 from aiogram import Bot, Dispatcher
 from config.config import Config, config
 from handlers import user, other
+from keyboards.menu import set_menu
 
 logger = logging.getLogger(__name__)
 
@@ -16,6 +17,10 @@ async def main():
 
     bot = Bot(token=configuration.bot.token)
     dp = Dispatcher()
+
+    await set_menu(bot)
+
+    # dp.include_routers(user.router, other.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
